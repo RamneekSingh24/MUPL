fd4451fa45a5c50389faa382abcd84f18366b509
RamneekSingh24 <ramneeksingh2001@gmail.com>
2021-11-11 03:59:58 +0530

    add rikudo solver

---
+++ Assignment4/README.md
@@ -0,0 +1,56 @@
+```bash
+| ?- [rikudo], notrace, solve(36, [[3,-3,2,-3],[0,-3,-1,-2],[-1,-1,0,-1],[-2,0,-1,0],[1,1,2,1]],[[3,-2,1],[1,-3,5],[1,-2,9],[-1,1,14],[0,2,16],[1,-1,19], [3,-1,22], [1,2,26], [-2,2,31],[-3,2,32], [-2,-1,36]]).
+The debugger is switched off
+compiling /rikudo.pl for byte code...
+/rikudo.pl compiled, 156 lines read - 14771 bytes written, 11 ms
+The debugger is switched off
+1 at (3, -2)
+2 at (2, -2)
+3 at (3, -3)
+4 at (2, -3)
+5 at (1, -3)
+6 at (0, -3)
+7 at (-1, -2)
+8 at (0, -2)
+9 at (1, -2)
+10 at (0, -1)
+11 at (-1, -1)
+12 at (-2, 0)
+13 at (-1, 0)
+14 at (-1, 1)
+15 at (-1, 2)
+16 at (0, 2)
+17 at (0, 1)
+18 at (1, 0)
+19 at (1, -1)
+20 at (2, -1)
+21 at (2, 0)
+22 at (3, -1)
+23 at (3, 0)
+24 at (2, 1)
+25 at (1, 1)
+26 at (1, 2)
+27 at (0, 3)
+28 at (-1, 3)
+29 at (-2, 3)
+30 at (-3, 3)
+31 at (-2, 2)
+32 at (-3, 2)
+33 at (-2, 1)
+34 at (-3, 1)
+35 at (-3, 0)
+36 at (-2, -1)
+execution aborted
+| ?- 
+```
+
+Co-ordinates used.
+
+![](2021-11-11-03-40-32.png)
+
+![](2021-11-11-03-43-04.png)
+
+> Forgot to take ss on http://www.rikudo-puzzle.com/ :(
+
+
+
--- Assignment4/test.pl
+++ Assignment4/test.pl
@@ -1,114 +1,20 @@
-:- dynamic(parent/2).
-:- dynamic(father/2).
-:- dynamic(mother/2).
+adj(1,3).
+adj(2,3).
+adj(3,4).
 
 
-parent(arti, babli).
-parent(arti, bunty).
-parent(babli, chitra).
-parent(babli, chintan).
-parent(bunty, divya).
-parent(bunty, divesh).
-male(bunty).
-male(chintan).
-male(divesh).
 
-
-childless(P) :- 
-    findall(1, parent(P, _), []).
-
-% childless(P) :- 
-%     findall(1, parent(P, _), []);
-%     findall(1, mother(P, _), []);
-%     findall(1, father(P, _), []).
-
-
-update_rules :-
-    parent(X, Y),
-    male(X),
-    retract(parent(X, Y)),
-    asserta(father(X, Y)).
-
-update_rules :-
-    parent(X, Y),
-    retract(parent(X, Y)),
-    asserta(mother(X, Y)).
-    
-    
-
-tower_of_hanoi(X) :-
-    solve(X, left, right, centre).
-
-% solve(X, From, To, EmptyRod)
-% solve([], _, _, _).
-
-solve([X|[]], From, To, _) :-
-    format('Move disk ~w from ~w to ~w.\n', [X, From, To]).
-
-solve([H|T], From, To, EmptyRod) :- 
-    solve(T, From, EmptyRod, To),
-    format('Move disk ~w from ~w to ~w.\n', [H, From, To]),
-    solve(T, EmptyRod, To, From).
-
-
-check1([X1,Y1], X2, Y2) :-
-    \+ (X1 = X2),
-    \+ (Y1 = Y2),
-    D1 is X1 - X2,
-    D2 is Y1 - Y2,
-    \+ (abs(D1) =:= abs(D2)).
-
-checkAll([], _, _).
-
-checkAll([H|T], X, Y) :- 
-    check1(H, X, Y),
-    checkAll(T, X, Y).
 
 write_sol([]).
 
-write_sol([ [ Row, Col] | Rest]) :-
-    format('(~w, ~w)\n', [Row, Col]),
-    
+write_sol([ X|Rest]) :-
+    format('(~w)\n', [X]),
+    length(X, L),
+    % format('hi  len: ~w\n', [L]),  
     write_sol(Rest).
 
-track(0, _, N, CurrQueens) :-
-    N > 0,
-    write_sol(CurrQueens).
-
-track(X, 0, N, CurrQueens) :-
-   X > 0, 
-   NextX is X - 1,
-   track(NextX, N, N, CurrQueens).
-
-
-track(X, Y, N, CurrQueens) :-
-    X > 0,
-    Y > 0,
-    checkAll(CurrQueens, X, Y),
-    track(X, 0, N, [[X,Y] | CurrQueens]).
-
-track(X, Y, N, CurrQueens) :-
-    X > 0,
-    Y > 1,
-    NextY is Y - 1, 
-    track(X, NextY, N, CurrQueens).
-
-
-nQueens(N) :-
-    N > 0, 
-    track(N, N, N, []).
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
-
+print :-
+    findall(1, adj(_, _), X),
+    length(X, L),
+    format('len: ~w\n', [L]),    
+    write_sol(X).
